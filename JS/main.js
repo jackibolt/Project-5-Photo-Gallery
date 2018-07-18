1828826e153e3834dbc743fc80a4ab0c61a0d7c1
Jacki Bolt <bolt.jacqueline@gmail.com>
2018-07-17 23:04:59 -0400

    more search bar

--- JS/main.js
+++ JS/main.js
@@ -5,9 +5,32 @@ lightbox.option({
   'albumLabel': '',
 })
 
+// function live_search() {
 
-      const $input = $('#search_input');
-      console.log($input);
-      const $filter = $input.val().toLowerCase();
-      console.log($filter);
-      
+$('.search').keyup(function({
+
+  // Declare variables
+  const input = $('#search_input').val();
+  const input_value = input.toLowerCase();
+  console.log(input_value);
+
+
+}));
+//
+//
+// $('#search-button').click(live_search(){
+//   const search = $('#search').val();
+//   console.log(search.toLowerCase());
+// });
+// Loop through all list items, and hide those who don't match the search query
+// for (i = 0; i < $title.length; i++) {
+//     $img = $title[i].getAttribute("data-title")[0];
+//     if ($img.innerHTML.toLowerCase().indexOf($filter) > -1) {
+//         $img[i].style.display = "";
+//     } else {
+//         $img[i].style.display = "none";
+//     }
+// }
+//
+//
+// $('#search_input').on('keyup', myFunction());
