afd0a7bc8604bb5ed1884103f4c25c0f6ae2a766
Jacki Bolt <bolt.jacqueline@gmail.com>
2018-07-18 11:05:03 -0400

    more search adjustments

--- JS/main.js
+++ JS/main.js
@@ -8,34 +8,37 @@ lightbox.option({
 // function live_search() {
 
 $('.search').keyup(function(){
+  console.log(event.target);
 
-    //Image-box Variable
-  const $thumbnail = $('.list a');
+      //Image-box Variable
+    const $thumbnail = $('.list a');
 
-  $thumbnail.each(function(index, element){
+    $thumbnail.each(function(index, element){
+      console.log(event.target);
+        //Search Input variables
+      const $input = $('#search_input').val(); //get input
+      const $input_value = $input.toLowerCase(); //return lowercase
+      console.log($input_value)
 
-      //Input variables
-    const $input = $('#search_input').val();
-    const $input_value = $input.toLowerCase();
-    console.log($input_value)
+        //Caption variables
+      const $attr = $(element).attr('data-title'); //get attr
+      const $caption = $attr.toLowerCase(); //return lowercase
+      console.log($caption);
 
-      //Caption variables
-    const $attr = $(element).attr('data-title');
-    const $caption = $attr.toLowerCase();
-    console.log($caption);
+        //Image Variable
+      const $img = $('.image');
+      // console.log($img);
 
-      //Image Variable
-    const $img = $('a img');
-    console.log($img);
+        Filter
+      if ($caption.includes($input_value)){
+          // Display matching results
+        $img.style.display = "";
+        console.log('yes');
 
-      //Filter
-    if ($caption.includes($input_value)){
-      $img.style.display = '';
-      console.log('yes');
-    } else {
-      $img.css("display","none")
-      console.log('no');
-    }
-  });
+      } else {         // Hide non-matching results
+        $img.style.display = "none";
+        console.log('no');
+      }
+    });
 
 });
